0a2f37213c612402ac5808a9137ff2afe200373a
hotio <26902309+hotio@users.noreply.github.com>
2020-10-05 09:58:21 +0200

    skip ci

--- update.sh
+++ update.sh
@@ -32,15 +32,12 @@ elif [[ ${1} == "screenshot" ]]; then
     exit 0
 else
     version=$(curl -fsSL "https://repo.jellyfin.org/releases/server/ubuntu/unstable/server/" | grep -o ">jellyfin-server_.*-unstable_amd64.deb<" | sed -e 's/>jellyfin-server_//g' -e 's/-unstable_amd64.deb<//g' | sort -r | head -1)
-    [[ -z ${version} ]] && exit 1
+    [[ -z ${version} ]] && exit 0
     version_web=$(curl -fsSL "https://repo.jellyfin.org/releases/server/ubuntu/unstable/web/" | grep -o ">jellyfin-web_.*-unstable_all.deb<" | sed -e 's/>jellyfin-web_//g' -e 's/-unstable_all.deb<//g' | sort -r | head -1)
     [[ -z ${version_web} ]] && exit 1
     version_ffmpeg=$(curl -fsSL "https://repo.jellyfin.org/releases/server/ubuntu/ffmpeg/" | grep -o ">jellyfin-ffmpeg_.*-bionic_amd64.deb<" | sed -e 's/>jellyfin-ffmpeg_//g' -e 's/-bionic_amd64.deb<//g')
     [[ -z ${version_ffmpeg} ]] && exit 1
-    echo "VERSION=${version}" > VERSION
-    echo "WEB_VERSION=${version_web}" >> VERSION
-    echo "FFMPEG_VERSION=${version_ffmpeg}" >> VERSION
-    echo '{"version":"'"${version}"'"}' > VERSION.json
+    echo '{"version":"'"${version}"'","web_version":"'"${version_web}"'","ffmpeg_version":"'"${version_ffmpeg}"'"}' | jq . > VERSION.json
     version="${version}/${version_web}/${version_ffmpeg}"
     echo "##[set-output name=version;]${version}"
 fi
